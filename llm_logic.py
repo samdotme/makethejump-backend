68af57a10720cfa10df738e514c568f04ef51cde
Sam Schneider <sam.ak.schneider@gmail.com>
2024-07-24 12:45:27 +0800

    Add mistral repo.

--- llm_logic.py
+++ llm_logic.py
@@ -1,6 +1,16 @@
+from langchain_huggingface import HuggingFaceEndpoint
+
 class LlmLogicBrain:
     def __init__(self, pinecone_index_name):
+      repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+      self.llm = HuggingFaceEndpoint(
+          repo_id=repo_id,
+          temperature=0.2,
+      )
+      
+      print("Loaded LLM")
+      
       return None
 
-    def respond_with_chain(self, query):     
-      return "I'm not yet implemented. Don't worry, I'll be smart soon!"
+    def respond_with_chain(self, query):
+      return self.llm.invoke(query)
