6e4b3575a23cf01db5bdbb5f305ce452e25b1c94
Sam Schneider <sam.ak.schneider@gmail.com>
2024-07-26 17:45:50 +0800

    Add prompt template.

--- llm_logic.py
+++ llm_logic.py
@@ -1,6 +1,35 @@
+from langchain_huggingface import HuggingFaceEndpoint
+from langchain_core.prompts import PromptTemplate
+
 class LlmLogicBrain:
     def __init__(self, pinecone_index_name):
+      repo_id = "mistralai/Mistral-7B-Instruct-v0.2"
+      self.llm = HuggingFaceEndpoint(
+        repo_id=repo_id,
+        temperature=0.9
+      )
+      
+      print("Loaded LLM")
+      
       return None
 
-    def respond_with_chain(self, query):     
-      return "I'm not yet implemented. Don't worry, I'll be smart soon!"
+    def respond_with_chain(self, query): 
+      context = """You are a virtual assistant specialized in answering questions about cats. 
+      If a question is unrelated to cats, pets in general, or pet adoption, politely and cutely decline to answer, 
+      explaining that you only provide information about these topics. 
+      Keep your responses to a maximum of three sentences, and ensure they are concise and friendly"""
+      
+      template = """
+      Context: {context}
+
+      Question: {question}
+
+      Helpful Answer:"""
+
+      prompt = PromptTemplate.from_template(template)
+      
+      llm_chain = prompt | self.llm
+
+      # Example invocation:
+      return llm_chain.invoke({"context": context, "question": query})
+
